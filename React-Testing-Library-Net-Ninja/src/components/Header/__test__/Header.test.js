6880f91f36cf28f0eb4b3564c1bab91171c210f6
RuFrontEnd <k718198@gmail.com>
2022-06-05 20:50:43 +0800

    新增 getBy

--- React-Testing-Library-Net-Ninja/src/components/Header/__test__/Header.test.js
+++ React-Testing-Library-Net-Ninja/src/components/Header/__test__/Header.test.js
@@ -1,90 +1,37 @@
-import { render, screen } from '@testing-library/react';
-import Header from '../Header';
+import { render, screen } from "@testing-library/react";
+import Header from "../Header";
 
-describe("Header", () => {
-    it('should render same text passed into title prop', () => {
-        render(
-            <Header 
-              title="todo"
-            />
-        );
-        const h1Element = screen.getByText(/todo/i);
-        expect(h1Element).toBeInTheDocument();
-    });
-})
+// getBy => 如果只有 1 個回傳 pass / 1 個以上則回傳 error
+test("should render same text passed into title props", () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByText(/my header/i);
+  expect(headingElement).toBeInTheDocument();
+});
 
-// it('should render same text passed into title prop', () => {
-//     render(
-//         <Header 
-//           title="todo"
-//         />
-//     );
-//     const h1Element = screen.getByRole("heading");
-//     expect(h1Element).toBeInTheDocument();
-// });
+test("should render same text passed into title props", () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByRole("heading", { name: "My Header" });
+  // 標籤與 role 對照表 https://www.w3.org/TR/html-aria/#docconformance
+  // { name: "value" } => value 為 aria-label 值, 如果沒有則 fallback 為 content
+  // const headingElement = screen.getByRole("heading", { name: "my-aria-label" }); <Header /> 第8行開啟的話
+  expect(headingElement).toBeInTheDocument();
+});
 
-// it('should render same text passed into title prop', () => {
-//     render(
-//         <Header 
-//           title="todo"
-//         />
-//     );
-//     const h1Element = screen.getByRole("heading", { name: /todo/i });
-//     expect(h1Element).toBeInTheDocument();
-// });
+test("should render same text passed into title props", () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByTitle("Header");
+  expect(headingElement).toBeInTheDocument();
+});
 
-// it('should render same text passed into title prop', () => {
-//     render(
-//         <Header 
-//           title="todo"
-//         />
-//     );
-//     const h1Element = screen.getByTitle("Header");
-//     expect(h1Element).toBeInTheDocument();
-// });
+test("should render same text passed into title props", () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.getByTestId("header-1");
+  expect(headingElement).toBeInTheDocument();
+});
 
-// it('should render same text passed into title prop', () => {
-//     render(
-//         <Header 
-//           title="todo"
-//         />
-//     );
-//     const h2Element = screen.getByTestId("header-2");
-//     expect(h2Element).toBeInTheDocument();
-// });
-
-// // WITH FINDBY
-
-// it('should render same text passed into title prop', async () => {
-//     render(
-//         <Header 
-//           title="todo"
-//         />
-//     );
-//     const h1Element = await screen.findByText(/todo/i);
-//     expect(h1Element).toBeInTheDocument();
-// });
-
-// // WITH QUERYBY
-
-// it('should render same text passed into title prop', () => {
-//     render(
-//         <Header 
-//           title="todo"
-//         />
-//     );
-//     const h1Element = screen.queryByText(/dogs/i);
-//     expect(h1Element).not.toBeInTheDocument
-// });
-
-// // WITH GETALLBY
-
-// it('should render same text passed into title prop', () => {
-//     render(
-//         <Header 
-//           title="todo"
-//         />
-//     );
-//     const h1Elements = screen.getAllByText(/todo/i);
-//     expect(h1Elements.length).toBe(1);
-// });
+// findBy => getBy 非同步版本
+test("should render same text passed into title props", async () => {
+  render(<Header title="My Header" />);
+  const headingElement = screen.findByText(/my header/i);
+  expect(headingElement).toBeInTheDocument();
+});
