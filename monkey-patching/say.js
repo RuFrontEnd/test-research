1657136f6b0b00c4f6a7c910395961d25fe0a708
Luke_Xie <Luke_Xie@wistron.com>
2022-09-30 13:59:44 +0800

    monkey-patching - 新增註解

--- monkey-patching/say.js
+++ monkey-patching/say.js
@@ -4,6 +4,12 @@ let sayOneTwoThree = require("./sayOneTwoThree"),
 
 sayOneTwoThree.sayOneTwoThree = () => {
   console.log("456");
-};
+}; // 這樣會直接修改 sayOneTwoThree 的邏輯
+
+// 如果是寫成 let { sayOneTwoThree } = require("./sayOneTwoThree")
+// sayOneTwoThree.sayOneTwoThree = () => {
+//  console.log("456");
+// };
+// 這樣是只有在 say.js 的 sayOneTwoThree 邏輯會修改
 
 saySomething.saySomething();
