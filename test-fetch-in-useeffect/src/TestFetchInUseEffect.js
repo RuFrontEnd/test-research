0168b43a08fc48d1e0f7b7ea8662b0d16d48ef09
RuFrontEnd <k718198@gmail.com>
2022-10-19 21:52:33 +0800

    研究 getby findby queryby wait for 關係

--- test-fetch-in-useeffect/src/TestFetchInUseEffect.js
+++ test-fetch-in-useeffect/src/TestFetchInUseEffect.js
@@ -2,23 +2,31 @@ import { useEffect, useState } from "react";
 import { fetchAPI } from "./testFetch";
 
 function TestFetchInUseEffect() {
+  const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
   useEffect(() => {
     fetchAPI().then((json) => {
       setData(json);
+      setLoading(false);
     });
   }, []);
 
   return (
     <div className="test fetch in useEffect">
-      {data.map((dataItem) => (
-        <div data-testid={dataItem.title} key={dataItem.id}>
-          <p>id: {dataItem.id}</p>
-          <p>title: {dataItem.title}</p>
-          <p>userId: {dataItem.userId}</p>
-        </div>
-      ))}
+      {loading ? (
+        <>loading...</>
+      ) : (
+        <>
+          {data.map((dataItem) => (
+            <div data-testid={dataItem.title} key={dataItem.id}>
+              <p>id: {dataItem.id}</p>
+              <p>title: {dataItem.title}</p>
+              <p>userId: {dataItem.userId}</p>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 }
--- test-fetch-in-useeffect/src/TestFetchInUseEffect.test.js
+++ test-fetch-in-useeffect/src/TestFetchInUseEffect.test.js
@@ -4,38 +4,71 @@ import { fetchAPI as mockFetchAPI } from "./testFetch";
 
 jest.mock("./testFetch");
 
+const mockResolvedValue = [
+  {
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  },
+  {
+    userId: 2,
+    id: 2,
+    title: "quis ut nam facilis et officia qui",
+    completed: false,
+  },
+  {
+    userId: 3,
+    id: 3,
+    title: "fugiat veniam minus",
+    completed: false,
+  },
+];
+
 describe("TestFetchInUseEffect", () => {
-  it("test fetch in useEffect", async () => {
-    // console.log("mockFetchAPI", mockFetchAPI);
+  it("should render loading... using getBy", () => {
+    mockFetchAPI.mockResolvedValue(mockResolvedValue);
 
-    mockFetchAPI.mockResolvedValue([
-      {
-        userId: 1,
-        id: 1,
-        title: "delectus aut autem",
-        completed: false,
-      },
-      {
-        userId: 1,
-        id: 2,
-        title: "quis ut nam facilis et officia qui",
-        completed: false,
-      },
-      {
-        userId: 1,
-        id: 3,
-        title: "fugiat veniam minus",
-        completed: false,
-      },
-    ]);
+    render(<TestFetchInUseEffect />);
 
-    act(() => {
-      render(<TestFetchInUseEffect />);
-    });
+    expect(screen.getByText("loading...")).toBeInTheDocument();
+  });
+
+  it("should render 'delectus aut autem' using findBy and await for getBy", async () => {
+    mockFetchAPI.mockResolvedValue(mockResolvedValue);
+
+    render(<TestFetchInUseEffect />);
 
     await waitFor(() => {
-      expect(screen.getByTestId('delectus aut autem')).toBeInTheDocument()
-      screen.debug() // 斷言後使用才會出現正確 DOM => 須調研原因
+      expect(
+        screen.getByTestId(mockResolvedValue[0].title)
+      ).toBeInTheDocument();
     });
+
+    // 上下等價
+
+    expect(
+      await screen.findByTestId(mockResolvedValue[0].title)
+    ).toBeInTheDocument();
+  });
+
+  it("should not render loading... after using findby(or wait for)", async () => {
+    mockFetchAPI.mockResolvedValue(mockResolvedValue);
+
+    render(<TestFetchInUseEffect />);
+
+    expect(
+      await screen.findByTestId(mockResolvedValue[0].title)
+    ).toBeInTheDocument();
+
+    // 上下等價
+
+    // await waitFor(() => {
+    //   expect(
+    //     screen.getByTestId(mockResolvedValue[0].title)
+    //   ).toBeInTheDocument();
+    // });
+
+    expect(screen.queryByText("loading...")).not.toBeInTheDocument();
   });
 });
